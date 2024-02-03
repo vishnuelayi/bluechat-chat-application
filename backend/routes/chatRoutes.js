2f4a0a2d8092b62e7743f88be6e11bfa7fcd9c4b
Vishnu Elayi <vishnuelayi@gmail.com>
2024-02-03 13:15:54 +0530

    Revert "completed chatRoute"
    This reverts commit 1aefd288628ccba9d0011dda092ce63e93ee6ccf.

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,21 +1,14 @@
 const express = require("express");
-const {
-  accessChat,
-  getChats,
-  createGroup,
-  renameGroup,
-  addUserToGroup,
-  rmvUserToGroup,
-} = require("../controllers/chatController");
+const { accessChat, getChats, createGroup, renameGroup } = require("../controllers/chatController");
 const authMiddleware = require("../middleware/authMiddleware");
+
+
 
 const router = express.Router();
 
-router.post("/", authMiddleware, accessChat);
-router.get("/", authMiddleware, getChats);
-router.post("/create-group", authMiddleware, createGroup);
-router.put("/rename-group", authMiddleware, renameGroup);
-router.put("/add-user", authMiddleware, addUserToGroup);
-router.put("/remove-user", authMiddleware, rmvUserToGroup);
+router.post("/",authMiddleware, accessChat);
+router.get("/",authMiddleware, getChats);
+router.post("/create-group", authMiddleware, createGroup)
+router.put("/rename-group", authMiddleware, renameGroup)
 
 module.exports = router;
